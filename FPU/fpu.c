0440a64566eab926987a7488609b66e11b2744d7
TakedaHiromasa <reich893@yahoo.co.jp>
2020-12-15 17:53:58 +0900

    add: fmull module

--- FPU/fpu.c
+++ FPU/fpu.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
+void finit(){
+  __asm volatile(
+    ".arch pentium;         "
+    "finit;                 "
+    "fwait;                 "
+    ".arch i286             "
+  );
+}
+
 double faddl(double a, double b){
   double out;
 
@@ -10,6 +19,24 @@ double faddl(double a, double b){
     "fldl  %1;              " // %1 -> st0
     "fldl  %2;              " // %2 -> st0, %1 -> st1
     "fadd %%st(1), %%st(0); " // st1 + st0 -> st0
+    "fstpl %0;              " // st0 -> %0 & pop st0
+    "fwait;                 "
+    ".arch i286             "
+    : "=m" (out) : "m" (a), "m" (b)
+  );
+
+  return out;
+}
+
+double fmull(double a, double b){
+  double out;
+
+  __asm volatile(
+    ".arch pentium;         "
+    "finit;                 "
+    "fldl  %1;              " // %1 -> st0
+    "fldl  %2;              " // %2 -> st0, %1 -> st1
+    "fmul %%st(1), %%st(0); " // st1 + st0 -> st0
     "fstpl %0;              " // st0 -> %0 & pop st0
     "fwait;                 "
     ".arch i286             "
@@ -29,11 +56,16 @@ int main(void){
   printf("b=");
   scanf("%lf", &b);
 
-  out = faddl(a, b);
+  finit();
 
+  out = faddl(a, b);
   printf("FADD = %f\n", out);
 
+  out = fmull(a, b);
+  printf("FMUL = %f\n", out);
 
+
+  // LD-ST test
   int i = 1;
   __asm volatile(
     ".arch pentium; "
