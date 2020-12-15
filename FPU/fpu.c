c1353be968b366b477e124e262aa2ae16011c46e
TakedaHiromasa <reich893@yahoo.co.jp>
2020-12-15 15:22:02 +0900

    add: FPU test code

---
+++ FPU/fpu.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+  double a, b;
+  double out;
+
+  puts("Real number input:");
+  printf("a=");
+  scanf("%f", &a);
+  printf("b=");
+  scanf("%f", &b);
+
+  __asm volatile(
+    ".arch pentium;         "
+    "finit;                 "
+    "fld  %1;               " // %1 -> st0
+    "fld  %2;               " // %2 -> st0, %1 -> st1
+    "fadd %%st(1), %%st(0); " // st1 + st0 -> st0
+    "fstpl %0;              " // st0 -> %0 & pop st0
+    "fwait;                 "
+    ".arch i286             "
+    : "=m" (out) : "m" (a), "m" (b)
+  );
+
+  printf("FADD = %f\n", out);
+
+
+  int i = 1;
+  __asm volatile(
+    ".arch pentium; "
+    "finit; "
+    "fild  %1; "
+    "fstpl %0; "
+    "fwait; "
+    ".arch i286"
+    : "=m" (out) : "m" (i)
+  );
+
+  printf("LD_ST = %f\n", out);
+
+  return 0;
+}
