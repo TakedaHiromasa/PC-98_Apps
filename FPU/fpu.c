1bd28d23e368035b6f2ed9f1cffac8f1bc53678f
TakedaHiromasa <reich893@yahoo.co.jp>
2020-12-15 17:43:05 +0900

    add: faddl module

--- FPU/fpu.c
+++ FPU/fpu.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
+double faddl(double a, double b){
+  double out;
+
+  __asm volatile(
+    ".arch pentium;         "
+    "finit;                 "
+    "fldl  %1;              " // %1 -> st0
+    "fldl  %2;              " // %2 -> st0, %1 -> st1
+    "fadd %%st(1), %%st(0); " // st1 + st0 -> st0
+    "fstpl %0;              " // st0 -> %0 & pop st0
+    "fwait;                 "
+    ".arch i286             "
+    : "=m" (out) : "m" (a), "m" (b)
+  );
+
+  return out;
+}
+
 int main(void){
   double a, b;
   double out;
@@ -11,17 +29,7 @@ int main(void){
   printf("b=");
   scanf("%lf", &b);
 
-  __asm volatile(
-    ".arch pentium;         "
-    "finit;                 "
-    "fld  %1;               " // %1 -> st0
-    "fld  %2;               " // %2 -> st0, %1 -> st1
-    "fadd %%st(1), %%st(0); " // st1 + st0 -> st0
-    "fstpl %0;              " // st0 -> %0 & pop st0
-    "fwait;                 "
-    ".arch i286             "
-    : "=m" (out) : "m" (a), "m" (b)
-  );
+  out = faddl(a, b);
 
   printf("FADD = %f\n", out);
 
