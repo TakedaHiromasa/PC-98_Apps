aecdc7c22a7cc81a64d2e2d2d086e458e3b2984c
TakedaHiromasa <taisyou119@gmail.com>
2021-01-21 23:07:43 +0900

    update: matrix binary dumpで転置行列を出力できるようにした

--- MatrixBin_dump/main.c
+++ MatrixBin_dump/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define SIZE 512
 
@@ -14,6 +15,8 @@ int main(int argc, char *argv[]){
 
   printf("size = %dx%d\n", size, size);
 
+  float *out;
+  float *tmp  = malloc(sizeof(float) * size * size);
   float *data = malloc(sizeof(float) * size * size);
   float *dst  = malloc(sizeof(float) * size * size);
 
@@ -21,12 +24,24 @@ int main(int argc, char *argv[]){
     data[i] = i;
   }
 
+  /* 転置行列 */
+  if(argc > 3 && strcmp(argv[3], "-T") == 0){
+    for(int y=0; y<size; y++){
+      for(int x=0; x<size; x++){
+        tmp[x*size+y] = data[y*size+x];
+      }
+    }
+    out = tmp;
+  }else{
+    out = data;
+  }
+
   FILE * fp = NULL;
   if((fp=fopen(argv[1],"wb")) == NULL){
     printf("%sファイルのオープンに失敗\n", argv[1]);
     return -1;
   }
-  fwrite(data, sizeof(data[0]), size * size, fp);
+  fwrite(out, sizeof(out[0]), size * size, fp);
   fclose(fp);
 
   if((fp=fopen(argv[1],"rb")) == NULL){
