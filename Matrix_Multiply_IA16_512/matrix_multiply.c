f65efdcb11e2156f9598eda919133f6d012ecdff
TakedaHiromasa <taisyou119@gmail.com>
2021-01-22 05:26:22 +0900

    upgrade: MM_IA16_512 とりあえず動作することを確認🎉

--- Matrix_Multiply_IA16_512/matrix_multiply.c
+++ Matrix_Multiply_IA16_512/matrix_multiply.c
@@ -9,7 +9,7 @@
 #include "../00_include/dos_fileio.h"
 #include "../00_include/dos_timer.h"
 
-#define SIZE 32
+#define SIZE 512
 
 void put_vram(char __far *addr, int box_size, int off_y, int off_x, int y, int x, char val){
   unsigned int y_point = off_y + (y * box_size);
@@ -22,168 +22,129 @@ void put_vram(char __far *addr, int box_size, int off_y, int off_x, int y, int x
   }
 }
 
-int main(int argc, char *argv[]){
-  unsigned int a_seg, b_seg, c_seg, err;
-  float __far *a, *b, *c;
-
-  puts("\n=== alloc ===");
-  if(err = _dos_allocmem((long)sizeof(float) * 512 /16 + 1, &a_seg) != 0) return -11;
-  if(err = _dos_allocmem((long)sizeof(float) * 512 /16 + 1, &b_seg) != 0) return -12;
-  if(err = _dos_allocmem((long)sizeof(float) * 512 /16 + 1, &c_seg) != 0) return -13;
-
-  a = (float __far *)MK_FP(a_seg, 0x0000);
-  b = (float __far *)MK_FP(b_seg, 0x0000);
-  c = (float __far *)MK_FP(c_seg, 0x0000);
-
-  uint Abin_handle = 0;
-  Abin_handle = dos_fopen("hoge");
-  dos_fread((void __far *)a, sizeof(float), 512, Abin_handle); // 値を512個(1行分)読み込む
-  dos_fclose(Abin_handle);
-
-  for(int x=0; x<512; x++){
-    printf("%5.1f ", a[x]);
-  }
-}
-
 // int main(int argc, char *argv[]){
-//   unsigned int size = 0;
-//   unsigned int a_seg, b_seg, c_seg,cc_seg, err;
-//   float __far *a, *b, *c, *cc;
-
-//   size = (argc > 1) ? atoi(argv[1]) : SIZE;
-//   printf("[ %dx%d matrix ]\n", size, size);
-
-//   puts("\n=== memory check ===");
-//   _dos_allocmem(0xFFFF, &a_seg);
-//   long available_mem = (long)a_seg * 16;
-//   long data_size     = (long)sizeof(float) * size * size * 4;  // minimum size of overlay heap
-//   printf("available %lu bytes (%lu KB)\n", available_mem, available_mem/1024);
-//   printf("data_size %lu bytes (%lu KB)\n", data_size, data_size/1024);
-
-//   if(available_mem < data_size){
-//     puts("\e[31mERROR:Insufficient memory!!\e[m");
-//     return -1;
-//   }
+//   unsigned int a_seg, b_seg, c_seg, err;
+//   float __far *a, *b, *c;
 
 //   puts("\n=== alloc ===");
-//   if(err = _dos_allocmem((long)sizeof(float)*size*size/16, &a_seg) != 0) return -11;
-//   if(err = _dos_allocmem((long)sizeof(float)*size*size/16, &b_seg) != 0) return -12;
-//   if(err = _dos_allocmem((long)sizeof(float)*size*size/16, &c_seg) != 0) return -13;
-//   if(err = _dos_allocmem((long)sizeof(float)*size*size/16, &cc_seg) != 0) return -14;
+//   if(err = _dos_allocmem((long)sizeof(float) * 512 /16 + 1, &a_seg) != 0) return -11;
+//   if(err = _dos_allocmem((long)sizeof(float) * 512 /16 + 1, &b_seg) != 0) return -12;
+//   if(err = _dos_allocmem((long)sizeof(float) * 512 /16 + 1, &c_seg) != 0) return -13;
 
 //   a = (float __far *)MK_FP(a_seg, 0x0000);
 //   b = (float __far *)MK_FP(b_seg, 0x0000);
 //   c = (float __far *)MK_FP(c_seg, 0x0000);
-//   cc=(float __far *)MK_FP(cc_seg, 0x0000);
-//   printf("0x%lx 0x%lx 0x%lx\n", a, b, c);
 
-//   puts("\n=== main ===");
-//   printf("Running"); fflush(stdout);
-//   char __far *vram0_addr;
-//   char __far *vram1_addr;
-//   vram0_addr = (char __far *)MK_FP(0xa800, 0x0000);
-//   vram1_addr = (char __far *)MK_FP(0xb000, 0x0000);
-  
-//   // グラフィック使用開始
-//   asm ("mov $0x40, %ah");
-//   asm ("int $0x18");
-//   for(long i=0;i<size+8;i++){
-//     for(long j=0;j<size+2;j++){
-//         put_vram(vram1_addr, 2, 10-8, 320-1, i, j, 0xff);
+//   uint Abin_handle = 0;
+//   Abin_handle = dos_fopen("../MM512BIN/B_T.bin");
+//   for (int i = 0; i < 2; i++){
+//     dos_fread((void __far *)a, sizeof(float), 512, Abin_handle); // 値を512個(1行分)読み込む
+    
+//     for(int x=0; x<512; x++){
+//       printf("%5.1f ", a[x]);
 //     }
+//     puts("");
 //   }
-
-//   for(long i=0;i<size;i++){
-//     for(long j=0;j<size;j++){
-//         put_vram(vram1_addr, 2, 10, 320, i, j, 0x00);
-//     }
-//   }
-
-//   printf(" -> Array init"); fflush(stdout);
-//   TIME start_time, end_time, seed_time;
-//   get_time(&seed_time);
-//   srand((&seed_time)->sec);
-//   for(long i=0; i<size*size; i++){
-//     // printf("0x%lx 0x%lx 0x%lx %d\n", &a[i], &b[i], &c[i], i);
-//     a[i]=(rand()%1001/500.0)-1;
-//     b[i]=(rand()%1001/500.0)-1;
-//     c[i]=cc[i]=0;
-//   }
-
-//   printf(" -> Matrix multiply\n"); fflush(stdout);
-//   // FPUを使用する場合
-//   // 必ず最初にfinitすること！
-//   finit();
-
-//   get_time(&start_time);
-//   for(long i=0;i<size;i++){
-//     for(long j=0;j<size;j++){
-//       finit();
-//       zeroset(); // zero set
-//       long c_idx=i*size+j;
-//       for(long k=0;k<size;k++){
-//         long a_idx=i*size+k;
-//         long b_idx=k*size+j;
-//         //c[c_idx]+=a[a_idx]*b[b_idx];
-//         fma_r(a[a_idx], b[b_idx]);
-//       }
-//       c[c_idx] = fpop();
-//       put_vram(vram0_addr, 2, 10, 320, i, j, 0xff);
-//     }
-//   }
-//   get_time(&end_time);
-//   for(long i=0;i<size;i++){
-//     for(long j=0;j<size;j++){
-//       for(long k=0;k<size;k++){
-//         long a_idx=i*size+k;
-//         long b_idx=k*size+j;
-//         long c_idx=i*size+j;
-//         cc[c_idx]+=a[a_idx]*b[b_idx];
-//       }
-//     }
-//   }
-
-//   float ave=0;
-//   float min=1000000;
-//   float max=-1000000;
-//   float lmin=1000000;
-//   float lmax=-1000000;
-//   float err_sum=0;
-//   for(int i=0;i<size*size;i++){
-//     min = fminf(fabs(c[i]-cc[i]),min);
-//     max = fmaxf(fabs(c[i]-cc[i]),max);
-//     lmin = fminf(fabs(c[i]-cc[i])/fabs(cc[i]),lmin);
-//     lmax = fmaxf(fabs(c[i]-cc[i])/fabs(cc[i]),lmax);
-//     err_sum+=fabs(c[i]-cc[i]);
-//     ave+=cc[i];
-//   }
-//   err_sum=(float)err_sum/(size*size);
-//   ave=ave/(size*size);
-//   printf("absolute min error:%le\n",min);
-//   printf("absolute max error:%le\n",max);
-//   printf("relative min error:%le\n",lmin);
-//   printf("relative max error:%le\n",lmax);
-//   printf("error average:%le\n",err_sum);
-//   printf("average:%le\n",ave);
-
-  
-//   printf("c[0]=%f\n", c[0]);
-//   printf("cc[0]=%f\n", cc[0]);
-//   printf("start_");
-//   print_time(&start_time);
-//   printf("  end_");
-//   print_time(&end_time);
-//   print_difftime(&start_time, &end_time);
-
-//   for(int i=0; i<atoi(argv[2]); i++){
-//     printf("\a");
-//   }
-
-//   // print calculation result 
-//   /* for(int i=0;i<size;i++){ */
-//   /*   for(int j=0;j<size;j++){ */
-//   /*     printf("%f,",c[i*size+j]); */
-//   /*   } */
-//   /*   printf("\n"); */
-//   /* } */
+//   dos_fclose(Abin_handle);
 // }
+
+int main(int argc, char *argv[]){
+  unsigned int size = 0;
+  unsigned int a_seg, b_seg, c_seg,cc_seg, err;
+  float __far *a, *b, *c, *cc;
+
+  size = (argc > 1) ? atoi(argv[1]) : SIZE;
+  printf("[ %dx%d matrix ]\n", size, size);
+
+  puts("\n=== memory check ===");
+  _dos_allocmem(0xFFFF, &a_seg);
+  long available_mem = (long)a_seg * 16;
+  long data_size     = (long)sizeof(float) * size * size * 4;  // minimum size of overlay heap
+  printf("available %lu bytes (%lu KB)\n", available_mem, available_mem/1024);
+  printf("data_size %lu bytes (%lu KB)\n", data_size, data_size/1024);
+
+  if(available_mem < size){
+    puts("\e[31mERROR:Insufficient memory!!\e[m");
+    return -1;
+  }
+
+  puts("\n=== alloc ===");
+  if(err = _dos_allocmem((long)sizeof(float) * size/16, &a_seg) != 0) return -11;
+  if(err = _dos_allocmem((long)sizeof(float) * size/16, &b_seg) != 0) return -12;
+  if(err = _dos_allocmem((long)sizeof(float) * size/16, &c_seg) != 0) return -13;
+  if(err = _dos_allocmem((long)sizeof(float) * size/16, &cc_seg) != 0) return -14;
+
+  a  = (float __far *)MK_FP(a_seg, 0x0000);
+  b  = (float __far *)MK_FP(b_seg, 0x0000);
+  c  = (float __far *)MK_FP(c_seg, 0x0000);
+  cc = (float __far *)MK_FP(cc_seg, 0x0000);
+  printf("0x%lx 0x%lx 0x%lx\n", a, b, c);
+
+  puts("\n=== main ===");
+  printf("Running"); fflush(stdout);
+  char __far *vram0_addr;
+  char __far *vram1_addr;
+  vram0_addr = (char __far *)MK_FP(0xa800, 0x0000);
+  vram1_addr = (char __far *)MK_FP(0xb000, 0x0000);
+  
+  // グラフィック使用開始
+  asm ("mov $0x40, %ah");
+  asm ("int $0x18");
+  for(long i=0;i<(size/4+8);i++){
+    for(long j=0;j<(size/4+2);j++){
+        put_vram(vram1_addr, 2, 10-8, 320-1, i, j, 0xff);
+    }
+  }
+
+  for(long i=0;i<size/4;i++){
+    for(long j=0;j<size/4;j++){
+        put_vram(vram1_addr, 2, 10, 320, i, j, 0x00);
+    }
+  }
+
+
+  printf(" -> Matrix multiply\n"); fflush(stdout);
+  TIME start_time, end_time, seed_time;
+  uint Abin_handle = dos_fopen("../MM512BIN/A.bin");
+  uint Cbin_handle = dos_fopen("C.bin");
+  finit();
+
+  get_time(&start_time);
+  for(long i=0;i<size;i++){
+    dos_fread((void __far *)a, sizeof(float), size, Abin_handle); // 値を512個(1行分)読み込む
+    uint Bbin_handle = dos_fopen("../MM512BIN/B_T.bin");
+    for(long j=0;j<size;j++){
+      dos_fread((void __far *)b, sizeof(float), size, Bbin_handle); // 値を512個(1行分)読み込む
+      finit();
+      zeroset(); // zero set
+      long c_idx = j; //i*size+j
+      c[c_idx] = 0.0;
+      for(long k=0; k<size; k++){
+        long a_idx = k; //i*size+k
+        long b_idx = k; //k*size+j
+        // c[c_idx] = fadd(c[c_idx], fmul(a[a_idx], b[b_idx])); // こちらの方が誤差が小さい
+        fma_r(a[a_idx], b[b_idx]);
+      }
+      c[c_idx] = fpop();
+      put_vram(vram0_addr, 2, 10, 320, i/4, j/4, 0xff);
+    }
+    dos_fclose(Bbin_handle);
+
+    dos_fwrite((void __far *)c, sizeof(float), size, Cbin_handle);
+  }
+  get_time(&end_time);
+
+  dos_fclose(Abin_handle);
+  dos_fclose(Cbin_handle);
+
+  // printf("c[0] = %f\n", c[0]);
+  printf("start_");
+  print_time(&start_time);
+  printf("  end_");
+  print_time(&end_time);
+  print_difftime(&start_time, &end_time);
+
+  for(int i=0; i<atoi(argv[2]); i++){
+    printf("\a");
+  }
+}
